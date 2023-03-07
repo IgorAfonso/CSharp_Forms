7e0508e084e6e5ba0e40106024392c9801e65d34
Igor Henrique Afonso <igor.afonso@supley.uti>
2023-03-07 07:44:39 -0300

    Validations and MainForm_Start

---
+++ CursoWindowsForms/Frm_Principal.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CursoWindowsForms
+{
+    partial class Frm_Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Btn_DemonstracaoKey = new Button();
+            Btn_HelloWorld = new Button();
+            Btn_Mascara = new Button();
+            Btn_ValidaCPF = new Button();
+            Btn_ValidaCPF2 = new Button();
+            Btn_ValidaSenha = new Button();
+            SuspendLayout();
+            // 
+            // Btn_DemonstracaoKey
+            // 
+            Btn_DemonstracaoKey.Location = new Point(12, 12);
+            Btn_DemonstracaoKey.Name = "Btn_DemonstracaoKey";
+            Btn_DemonstracaoKey.Size = new Size(114, 48);
+            Btn_DemonstracaoKey.TabIndex = 0;
+            Btn_DemonstracaoKey.Text = "Demonstração Key";
+            Btn_DemonstracaoKey.UseVisualStyleBackColor = true;
+            // 
+            // Btn_HelloWorld
+            // 
+            Btn_HelloWorld.Location = new Point(132, 12);
+            Btn_HelloWorld.Name = "Btn_HelloWorld";
+            Btn_HelloWorld.Size = new Size(114, 48);
+            Btn_HelloWorld.TabIndex = 1;
+            Btn_HelloWorld.Text = "Hello World";
+            Btn_HelloWorld.UseVisualStyleBackColor = true;
+            // 
+            // Btn_Mascara
+            // 
+            Btn_Mascara.Location = new Point(252, 12);
+            Btn_Mascara.Name = "Btn_Mascara";
+            Btn_Mascara.Size = new Size(114, 48);
+            Btn_Mascara.TabIndex = 2;
+            Btn_Mascara.Text = "Máscara";
+            Btn_Mascara.UseVisualStyleBackColor = true;
+            // 
+            // Btn_ValidaCPF
+            // 
+            Btn_ValidaCPF.Location = new Point(12, 66);
+            Btn_ValidaCPF.Name = "Btn_ValidaCPF";
+            Btn_ValidaCPF.Size = new Size(114, 48);
+            Btn_ValidaCPF.TabIndex = 3;
+            Btn_ValidaCPF.Text = "Valida CPF";
+            Btn_ValidaCPF.UseVisualStyleBackColor = true;
+            // 
+            // Btn_ValidaCPF2
+            // 
+            Btn_ValidaCPF2.Location = new Point(132, 66);
+            Btn_ValidaCPF2.Name = "Btn_ValidaCPF2";
+            Btn_ValidaCPF2.Size = new Size(114, 48);
+            Btn_ValidaCPF2.TabIndex = 4;
+            Btn_ValidaCPF2.Text = "Valida CPF 2";
+            Btn_ValidaCPF2.UseVisualStyleBackColor = true;
+            // 
+            // Btn_ValidaSenha
+            // 
+            Btn_ValidaSenha.Location = new Point(252, 66);
+            Btn_ValidaSenha.Name = "Btn_ValidaSenha";
+            Btn_ValidaSenha.Size = new Size(114, 48);
+            Btn_ValidaSenha.TabIndex = 5;
+            Btn_ValidaSenha.Text = "Valida Senha";
+            Btn_ValidaSenha.UseVisualStyleBackColor = true;
+            // 
+            // Frm_Principal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(377, 126);
+            Controls.Add(Btn_ValidaSenha);
+            Controls.Add(Btn_ValidaCPF2);
+            Controls.Add(Btn_ValidaCPF);
+            Controls.Add(Btn_Mascara);
+            Controls.Add(Btn_HelloWorld);
+            Controls.Add(Btn_DemonstracaoKey);
+            Name = "Frm_Principal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Principal";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button Btn_DemonstracaoKey;
+        private Button Btn_HelloWorld;
+        private Button Btn_Mascara;
+        private Button Btn_ValidaCPF;
+        private Button Btn_ValidaCPF2;
+        private Button Btn_ValidaSenha;
+    }
+}
--- CursoWindowsForms/Frm_ValidaCPF_2.cs
+++ CursoWindowsForms/Frm_ValidaCPF_2.cs
@@ -20,28 +20,49 @@ namespace CursoWindowsForms
         private void Btn_Valida_Click(object sender, EventArgs e)
         {
 
-            if (MessageBox.Show("Você deseja realmente validar o CPF?",
-                "Mensagem de Validação", MessageBoxButtons.YesNo,
-                MessageBoxIcon.Question) == DialogResult.Yes)
+            string vConteudo;
+            vConteudo = Msk_CPF.Text;
+            vConteudo = vConteudo.Replace(".", "").Replace("-", "");
+            vConteudo = vConteudo.Trim();
+
+            if(vConteudo == "") 
+            {
+                MessageBox.Show("Você deve digitar um CPF", "Mensagem de validação",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else 
             {
 
-                bool result;
-                result = Cls_Uteis.Valida(Msk_CPF.Text);
+                if(vConteudo.Length != 11)
+                {
+                    MessageBox.Show("CPF deve ter 11 dígitos", "Mensagem de validação", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                } 
+                else 
+                {
+                    if (MessageBox.Show("Você deseja realmente validar o CPF?",
+               "Mensagem de Validação", MessageBoxButtons.YesNo,
+               MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
 
-                if (result == true)
-                {
-                    MessageBox.Show("CPF Válido!", 
-                    "Mensagem de Validação", MessageBoxButtons.OK,
-                    MessageBoxIcon.Information);
-                }
-                else
-                {
-                    MessageBox.Show("CPF Inválido!", 
-                    "Mensagem de Validação", MessageBoxButtons.OK,
-                    MessageBoxIcon.Error);
+                        bool result;
+                        result = Cls_Uteis.Valida(Msk_CPF.Text);
+
+                        if (result == true)
+                        {
+                            MessageBox.Show("CPF Válido!",
+                            "Mensagem de Validação", MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("CPF Inválido!",
+                            "Mensagem de Validação", MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                        }
+                    }
                 }
             }
-
         }
 
         private void Btn_Reset_Click(object sender, EventArgs e)
